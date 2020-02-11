4cec14e88a762759a79e2c0f6f1536202d4dcd4b
Aaron Cherian <cheri023@d.umn.edu>
2020-02-10 23:49:25 -0600

    added excel sheet for graphing purposes

---
+++ eulerMethod.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+/** This is a program that implements Euler's Method as part
+*   Chapter 2.4: Numerical Approximation.
+
+MATH 3280: Differential Equations and Linear Algebra.
+
+Equation:
+
+dy/dx = y*e^(-x^2)
+
+@author Aaron Cherian
+@date Friday Feb 7th, 2020
+*/
+
+float diffEquation(float x, float y)
+{
+    return y*(pow(exp(1), pow(-x,2))); // ye^(-x^2)
+}
+
+int main ()
+{
+    float xInitial = 0;
+    float yInitial = 1;
+    float h1 = 0.001;
+    float h2 = 0.0001;
+    float x = 0; //starting interval
+    float y = 1; //ending interval
+    float temp;
+    ofstream myfile;
+    myfile.open ("example.txt");
+    myfile << "\n x-values\t  y-values\n";
+    while(xInitial < y){
+        temp = h1*diffEquation(xInitial,yInitial);
+        yInitial = yInitial + temp;
+        xInitial = xInitial + h1;
+        //temp = y;
+        //y = y + h1*diffEquation(xInitial,y);
+        //xInitial = xInitial + h1;
+
+        myfile << yInitial << endl;// "          " << yInitial << endl;
+    }
+    myfile.close();
+
+}
+
